8551ffdc5f4c23bf09a34d83de6265fb98825a6f
johndavey <john.n.davey@gmail.com>
2017-10-16 17:48:13 -0400

    Add material UI and basic app routing/nav

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,27 +1,72 @@
 import React, { Component } from 'react'
 // import { connect } from 'react-redux'
-
+import { AppBar,
+    Toolbar,
+    IconButton,
+    MenuIcon,
+    Typography,
+    List,
+    ListItem,
+    ListItemText,
+    ListItemIcon,
+    ListSubheader } from 'material-ui';
+import { Route, Link } from 'react-router-dom'
 import '../App.css';
 
 class App extends Component {
 
+    state = {
+        categories: []
+    }
+
+    componentDidMount(){
+    	fetch('http://localhost:3001/categories', { headers: {Authorization: 'shbdkjn'}})
+        .then(response => response.json())
+        .then(response => {
+            this.setState({categories: response.categories});
+        });
+	}
+
+    render() {
+        console.log(this.state.categories);
+        const categories = this.state.categories;
+        let categoriesTitle = categories.length > 0 ? 'Categories' : 'No categories found.';
+        return (
+            <div className="App">
+                <AppBar position="static">
+                    <Toolbar>
+                        <Typography type="title">
+                            Readable
+                        </Typography>
+                    </Toolbar>
+                </AppBar>
+
+            <Route exact path='/'>
+                <List subheader={<ListSubheader>{categoriesTitle}</ListSubheader>}>
+                    {categories.length > 0 && categories.map(cat => (
+                        <Link to={"/" + cat.name}>
+                            <ListItemIcon>
+                                
+                            </ListItemIcon>
+                            <ListItem button>
+                                <ListItemText inset primary={ cat.name } />
+                            </ListItem>
+                        </Link>
+                    ))}
+                </List>
+            </Route>
 
 
+            {categories.length > 0 && categories.map(cat => (
+                <Route key={cat.name} path={'/' + cat.path} render={({history}) =>
+                        <div className="Hi">{ cat.name }</div>
+                }
+                />))
+            }
 
-
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+            </div>
+        );
+    }
 }
 
 export default App;
